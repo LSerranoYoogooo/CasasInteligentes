ba1ffc4d47eb9ed26bac8df0ea8ae0456a1c328a
Luis Serrano <lserrano@yoogooo.com>
2017-01-10 15:17:15 -0600

    initail page

--- App_movil/YoogoooSmartHome/app/src/main/java/com/yoogooo/yoogooosmarthome/Login.java
+++ App_movil/YoogoooSmartHome/app/src/main/java/com/yoogooo/yoogooosmarthome/Login.java
@@ -1,7 +1,13 @@
 package com.yoogooo.yoogooosmarthome;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //boton fotante para agregar nuevos usuarios
+        FloatingActionButton addUsr = (FloatingActionButton) findViewById(R.id.addUsr);
+        addUsr.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        //boton de inicio de sesion
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Main.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }
