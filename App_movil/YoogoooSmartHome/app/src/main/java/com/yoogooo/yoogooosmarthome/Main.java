219bbc5b11863c5579f270a789ac885e8a8f059c
Luis Serrano <lserrano@yoogooo.com>
2017-01-17 13:51:39 -0600

    add voice control

--- App_movil/YoogoooSmartHome/app/src/main/java/com/yoogooo/yoogooosmarthome/Main.java
+++ App_movil/YoogoooSmartHome/app/src/main/java/com/yoogooo/yoogooosmarthome/Main.java
@@ -1,13 +1,58 @@
 package com.yoogooo.yoogooosmarthome;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Main extends AppCompatActivity {
+    TextView res;
+    private static final int RECOGNIZE_SPEECH_ACTIVITY = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        res = (TextView) findViewById(R.id.res);
+
+    }
+
+    //onclic pendiente del float button btnMic
+    public void onClickBtnMic(View v) {
+        Intent intentActionRecognizeSpeech = new Intent(
+                RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        // Configura el Lenguaje (Español-México)
+        intentActionRecognizeSpeech.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL, "es-MX");
+        try {
+            startActivityForResult(intentActionRecognizeSpeech, RECOGNIZE_SPEECH_ACTIVITY);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(), "Tú dispositivo no soporta el reconocimiento por voz", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //receptor de onClickBtnMic
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case RECOGNIZE_SPEECH_ACTIVITY:
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> speech = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String strSpeech2Text = speech.get(0);
+                    res.setText(strSpeech2Text);
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
